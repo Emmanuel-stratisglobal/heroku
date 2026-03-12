597227b7da398531f987336705fd7719af35ef4f
MannyET12 <Emmanuel@stratisglobal.com>
2026-03-12 17:34:36 +0200

    Added app list page

---
+++ Frontend/Pages/Apps/Index.cshtml.cs
@@ -0,0 +1,23 @@
+using System.Configuration;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GettingStarted.Frontend.Pages;
+
+public class IndexModel : PageModel
+{ 
+    public List<JsonElement> Apps { get; set; } = new();
+
+    public async Task OnGetAsync()
+    {
+        var apiKey = Environment.GetEnvironmentVariable("HEROKU_API_KEY");
+
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+        client.DefaultRequestHeaders.Add("Accept", "application/vnd.heroku+json; version=3");
+
+        var response = await client.GetAsync("https://api.heroku.com/apps");
+        var json = await response.Content.ReadAsStringAsync();
+        Apps = JsonSerializer.Deserialize<List<JsonElement>>(json) ?? new();
+    }
+}
